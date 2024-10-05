6ff97a5415b6658398a8939000899c3bfa0e98d8
Pierre Stordiau <pierrestordiau980@gmail.com>
2024-10-05 03:53:53 +0200

    Ajout de la boucle qui relance le programme

--- todo.sh
+++ todo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Bonjour, veuillez choisir une action à réaliser en tapant 1, 2 ou 3"
-echo "1 Afficher la liste des tâches"
-echo "2 Ajouter une tâche"
-echo "3 Supprimer une tâche"
+while true; do
+    echo "Bonjour, veuillez choisir une action à réaliser en tapant 1, 2 ou 3"
+    echo "1 Afficher la liste des tâches"
+    echo "2 Ajouter une tâche"
+    echo "3 Supprimer une tâche"
+    read -p "Ou voulez-vous quitter le programme ? (oui/non)" user_input
+    if [[ "$user_input" == "oui" ]]; then
+        echo "Fin du programme"
+        break
+    else
+        echo "la boucle continue"
+    fi
+done
