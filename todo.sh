32a7672f2bae18a3c8221a3430801e19e007c25e
Pierre Stordiau <pierrestordiau980@gmail.com>
2024-10-05 03:40:59 +0200

    Affichage du message d'accueil

---
+++ todo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Bonjour, veuillez choisir une action à réaliser en tapant 1, 2 ou 3"
+echo "1 Afficher la liste des tâches"
+echo "2 Ajouter une tâche"
+echo "3 Supprimer une tâche"
