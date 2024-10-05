7be2aaf37408de74dbb77b15e10acf0c2656d5fc
Pierre Stordiau <pierrestordiau980@gmail.com>
2024-10-05 21:14:47 +0200

    ajout de commentaire

--- todo.sh
+++ todo.sh
@@ -8,7 +8,7 @@ while true; do # Boucle infinie pour relancer le programme tant que l'utilisateu
     echo "4. Quitter le programme"
     echo "5. Supprimer toutes les tâches"
     read -p "Veuillez entrer votre choix : " user_input # Récupération du choix de l'utilisateur
-    if [[ "$user_input" == "1" ]]; then
+    if [[ "$user_input" == "1" ]]; then # Si l'utilisateur a choisi le 1 il voit les tâches s'afficher sinon on lui indique qu'il n'y en a pas
         echo "Voici la liste des tâches actuelles"
         if [[ ! -s $task_file ]]; then
             echo "Aucune tâche trouvée."
