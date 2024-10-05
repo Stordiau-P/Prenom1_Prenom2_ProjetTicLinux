df9503656fc0b486ab2dd4745d2f325a854cd8dc
Pierre Stordiau <pierrestordiau980@gmail.com>
2024-10-05 17:34:23 +0200

    correction des erreurs et amélioration du code

--- todo.sh
+++ todo.sh
@@ -2,20 +2,25 @@
 
 while true; do
     echo "Bonjour, veuillez choisir une action à réaliser en tapant 1, 2 ou 3"
-    echo "1 Afficher la liste des tâches"
-    echo "2 Ajouter une tâche"
-    echo "3 Supprimer une tâche"
-    read -p "Ou voulez-vous quitter le programme ? (oui/non)" user_input
-    if [[ "$user_input" == "oui" ]]; then
-        echo "Fin du programme"
-        break
-    elif [[$user_input == "1"]]; then
+    echo "1. Afficher la liste des tâches"
+    echo "2. Ajouter une tâche"
+    echo "3. Supprimer une tâche"
+    echo "4. Quitter le programme"
+    read -p "Veuillez entrer votre choix : " user_input
+    if [[ "$user_input" == "1" ]]; then
         echo "Voici la liste des tâches actuelles"
-    elif [[$user_input == "2"]]; then
-        echo "Veuillez écrire la tâche à executer"
-    elif [[$user_input == "3"]]; then
-        echo "Veuillez indiquer la tache à supprimer"
-    else
-        echo "la boucle continue"
+    elif [[ "$user_input" == "2" ]]; then
+        read -p "Veuillez écrire la tâche à exécuter : " task
+        echo "Ajout de la tâche : $task"
+    elif [[ "$user_input" == "3" ]]; then
+        echo "Veuillez indiquer la tâche à supprimer"
+    elif [[ "$user_input" == "4" ]]; then
+        read -p "Voulez-vous quitter le programme ? (oui/non) " quit_choice
+        if [[ "$quit_choice" == "oui" ]]; then
+            echo "Fin du programme"
+            break
+        else
+            echo "La boucle continue"
+        fi
     fi
 done
